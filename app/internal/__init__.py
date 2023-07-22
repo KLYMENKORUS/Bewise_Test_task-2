5a4519df7d52f00306074c3f35de6b1314039fe7
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-07-22 14:53:30 +0300

    changed the form to add audio

--- app/internal/__init__.py
+++ app/internal/__init__.py
@@ -1,4 +1,4 @@
 from .api_v1.schemas import UserRead, UserCreate
 from .api_v1.handlers import router as router_handlers
-from .api_v1.handlers import add_music
+from .api_v1.handlers import add_music, all_audio_by_user
 from .pages.router import router as router_pages
--- app/internal/pages/router.py
+++ app/internal/pages/router.py
@@ -1,7 +1,7 @@
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, Request, Depends
 
-from app.internal import add_music
+from app.internal import add_music, all_audio_by_user
 
 
 router = APIRouter(prefix='/pages', tags=['Pages'])
@@ -9,12 +9,16 @@ router = APIRouter(prefix='/pages', tags=['Pages'])
 template = Jinja2Templates(directory='app/internal/templates')
 
 
-@router.get('/add')
-def home(request: Request):
-    return template.TemplateResponse('index.html', {'request': request})
+@router.get('/add_audio')
+def home(request: Request, record_all=Depends(all_audio_by_user)):
+    return template.TemplateResponse(
+        'index.html', {'request': request, 'record_all': record_all}
+    )
 
 
-@router.post('/add')
+@router.post('/add_audio')
 def add_audio(request: Request, audio_file=Depends(add_music)):
-    return template.TemplateResponse('index.html', {'request': request, 'audio': audio_file})
+    return template.TemplateResponse(
+        'index.html', {'request': request, 'audio': audio_file}
+    )
 
