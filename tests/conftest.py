e603d76e73a530454c982ad0a2dbd8a84c79e4e3
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-07-28 23:24:30 +0300

    some changes in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
 import asyncio
+from typing import AsyncGenerator
 
 import pytest
-from typing import AsyncGenerator
 from httpx import AsyncClient
 from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine,\
     async_sessionmaker, AsyncSession
+from sqlalchemy import insert, text
 
 from app import create_app
 from app.database.session import get_db
-from app.database import metadata
+from app.database import metadata, User
+from app.utils.hashing import Hasher
 
 
 app = create_app()
 
 
-DATABASE_URL_TEST = 'postgresql+asyncpg://postgres_test:postgres_test@db_test:5432/Testdb'
+DATABASE_URL_TEST = 'postgresql+asyncpg://postgres_test:postgres_test@127.0.0.1:5436/Testdb'
 
 # create async test engine
 test_engine = create_async_engine(
@@ -60,6 +62,40 @@ async def client() -> AsyncGenerator[AsyncClient, None]:
     """Create a new FastAPI TestClient instance"""
     async with AsyncClient(app=app, base_url='http://test') as async_client:
         yield async_client
+
+
+@pytest.fixture(scope='session', autouse=True)
+async def async_session_test():
+    """Test async session"""
+    engine = create_async_engine(
+        DATABASE_URL_TEST, future=True, echo=True, poolclass=NullPool
+    )
+    async_session_maker = async_sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession)
+    yield async_session_maker
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def clean_tables(async_session_test):
+    """Clean data in all tables before running test function"""
+    async with async_session_test() as session:
+        for table_for_cleaning in ['user']:
+            await session.execute(text(f'TRUNCATE TABLE "{table_for_cleaning}" CASCADE;'))
+
+
+@pytest.fixture
+async def create_user_in_database(async_session_test):
+    async def create_user_in_database(**kwargs):
+        password = kwargs.pop('password')
+        hashed_password = Hasher.get_hashed_pass(password)
+        kwargs.update(hashed_password=hashed_password)
+
+        async with async_session_test() as session:
+            result = await session.execute(insert(User).values(kwargs).returning(User))
+            return result.scalars().first()
+
+    return create_user_in_database
+
 
 
 
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import aiofiles
 from http import HTTPStatus
 from httpx import AsyncClient
 
@@ -7,27 +8,54 @@ BASE_DIR = Path(__file__).parent
 AUDIO_FILE = BASE_DIR / 'sample-6s.wav'
 
 
-async def test_create_user(client: AsyncClient):
+async def test_create_user(client: AsyncClient, create_user_in_database):
     new_user = {
-        "email": "user@example.com",
+        "email": "user1@example.com",
         "password": "12345",
         "is_active": True,
         "is_superuser": False,
         "is_verified": False,
         "username": "string"
     }
-    response = await client.post(url='/auth/register', json=new_user)
+    response = await client.post(url='user/register', json=new_user)
 
-    assert response.status_code == HTTPStatus.CREATED
-    assert response.json()['is_active'] is True, response.text
-
-    return response.json().get('email')
+    assert response.status_code == 200
+    assert response.json().get('is_active') is True
 
 
 async def test_login(client: AsyncClient):
-    response = await client.post(url='auth/jwt/login',
-                                 data={'username': 'user@example.com', 'password': '12345'},
+    response = await client.post(url='user/login',
+                                 data={'username': 'user1@example.com', 'password': '12345'},
                                  headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
-    assert response.status_code == HTTPStatus.NO_CONTENT
-    assert response.headers.get('Set-Cookie') is not None, response.text
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in response.json()
+
+
+async def test_add_file(
+        client: AsyncClient, create_user_in_database):
+    new_user = {
+        "email": "user1@example.com",
+        "password": "12345",
+        "is_active": True,
+        "is_superuser": False,
+        "is_verified": False,
+        "username": "string"
+    }
+    user = await create_user_in_database(**new_user)
+
+    async def upload_file():
+        async with aiofiles.open(AUDIO_FILE, 'rb') as file:
+            audio_file = await file.read()
+
+        return audio_file
+
+    response = await client.post(
+        url=f'audio/add?email={user.email}', files={'audio_file': ('sample-6s.wav', await upload_file())}
+    )
+
+    assert response.status_code == HTTPStatus.OK, response.text
+
+
+
+
